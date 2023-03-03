b7ae83a064ee37f56ee85823c6c8b342062eaf4d
sagar <sagar-iitg>
2023-03-03 15:59:58 +0530

    added Electronic store

---
+++ ElectronicStore/src/main/java/com/sk/dtos/JwtResponse.java
@@ -0,0 +1,29 @@
+/**
+ * @author
+ * Sagar Kumar
+ */
+package com.sk.dtos;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@ToString
+
+public class JwtResponse 
+{
+	private String jwtToken;
+	private UserDto user;
+	
+	
+	
+
+}
---
+++ ElectronicStore/src/main/java/com/sk/dtos/RoleDto.java
@@ -0,0 +1,25 @@
+/**
+ * @author
+ * Sagar Kumar
+ */
+package com.sk.dtos;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class RoleDto {
+
+	
+	
+	private String roleId;
+	private String roleName;
+	
+}
